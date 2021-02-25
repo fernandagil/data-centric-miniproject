d5d2874b2b0680c29a9d3fa1815f06821be39b96
fernandagil <fernandagilgonzalez@gmail.com>
2021-02-25 21:52:30 +0000

    Add task form input fields

--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,17 @@
-// vanilla JavaScript for MaterializeCSS initialization
-document.addEventListener('DOMContentLoaded', function () {
-    let sidenavs = document.querySelectorAll(".sidenav");
-    let sidenavsInstance = M.Sidenav.init(sidenavs, {edge: "right"});
-});
+/*
+    jQuery for MaterializeCSS initialization
+*/
 
-// Collapsible initialization
-$(document).ready(function(){
-    $('.collapsible').collapsible();
-});
-
-// Tooltips initialization
-$(document).ready(function(){
-    $('.tooltipped').tooltip();
+$(document).ready(function () {
+    $(".sidenav").sidenav({edge: "right"});
+    $(".collapsible").collapsible();
+    $(".tooltipped").tooltip();
+    $(".datepicker").datepicker({
+        format: "dd mmmm, yyyy",
+        yearRange: 3,
+        showClearBtn: true,
+        i18n: {
+            done: "Select"
+        }
+    });
 });
